756b8e58137a36e45466da6f3202c60a42ae7bba
oxor <carlroccobecker@gmail.com>
2022-07-20 17:17:09 +0200

    removed details, made tasks inline

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 class Todo(models.Model):
     title=models.CharField(max_length=100)
-    details=models.TextField()
     date=models.DateTimeField(default=timezone.now)
  
     def __str__(self):
